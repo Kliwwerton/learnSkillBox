e26232214231f49ed167f95620c672eb1d00f954
Alexander <AStrukov-96@yandex.ru>
2022-12-01 08:32:13 +0500

    Completed eight task

--- 2.1. Базовые структуры данных/Доп. материалы/lesson_002/08_garden.py
+++ 2.1. Базовые структуры данных/Доп. материалы/lesson_002/08_garden.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 # в саду сорвали цветы
@@ -10,24 +9,19 @@ meadow = ('клевер', 'одуванчик', 'ромашка', 'клевер'
 # создайте множество цветов, произрастающих в саду и на лугу
 # garden_set =
 # meadow_set =
-# TODO здесь ваш код
 garden_set = set(garden)
 meadow_set = set(meadow)
 
 # выведите на консоль все виды цветов
-# TODO здесь ваш код
-print(garden_set, meadow_set)
+print(1, garden_set | meadow_set)
 
 # выведите на консоль те, которые растут и там и там
-# TODO здесь ваш код
-print(garden_set | meadow_set)
+print(2, garden_set, meadow_set)
 
 # выведите на консоль те, которые растут в саду, но не растут на лугу
-# TODO здесь ваш код
-print(garden_set - meadow_set)
+print(3, garden_set - meadow_set)
 
 # выведите на консоль те, которые растут на лугу, но не растут в саду
-# TODO здесь ваш код
-print(meadow_set - garden_set)
+print(4, meadow_set - garden_set)
 
 
