a2edc93dfd359829cc07e3374ad1e4dd5eaab5e0
Alexander <AStrukov-96@yandex.ru>
2022-12-09 16:55:28 +0500

    Completed sixth task

--- 3.1 Основные операторы/Доп. материалы/ДЗ 3/06_rainbow.py
+++ 3.1 Основные операторы/Доп. материалы/ДЗ 3/06_rainbow.py
@@ -12,38 +12,35 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
 
 a, b, c, d = 50, 50, 1100, 750
-for i in rainbow_colors * 20:
+for i in rainbow_colors * 30:
     point_start = sd.get_point(a, b)
     point_finish = sd.get_point(c, d)
     step = 5
     sd.line(start_point=point_start, end_point=point_finish, color=i, width=4)
     a += step
     # b += step
-    c += step
+    c += step/12
     # d += step
 
-
-sd.vector(a, b, length=100, width=10)
-
-
 # Усложненное задание, делать по желанию.
-# Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
+# Нарисовать радугу дугами от окружности (см sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
 
 
-def bubble(point, radius, color, width):
-    sd.circle(center_position=point, radius=radius, width=width, color=color)
+# def bubble(point, radius, color, width):
+#     sd.circle(center_position=point, radius=radius, width=width, color=color)
 
 
 x = 700
 y = -300
+point = sd.get_point(x, y)
 radius = 1050
-for i in rainbow_colors * 1:
-    color = i
-    point = sd.get_point(x, y)
-    step2 = 10
+for i in rainbow_colors:
+    # color = i
+    step2 = 20
     width = step2
-    bubble(point, radius, color, width)
+    # bubble(point, radius, color, width)
+    sd.circle(center_position=point, radius=radius, width=width, color=i)
     radius -= step2
 
 sd.pause()
