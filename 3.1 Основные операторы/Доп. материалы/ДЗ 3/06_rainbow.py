72095cece4a036cc1566c7c077bfc97bd5235a2a
AStru <AStrukov-96@yandex.ru>
2022-12-10 22:43:50 +0500

    Completed seventh task (wall)

--- 3.1 Основные операторы/Доп. материалы/ДЗ 3/06_rainbow.py
+++ 3.1 Основные операторы/Доп. материалы/ДЗ 3/06_rainbow.py
@@ -3,23 +3,27 @@
 # (цикл for)
 
 import simple_draw as sd
-sd.background_color = sd.COLOR_DARK_BLUE
-sd.resolution = (1200, 800)
+
+x = 1200
+y = 800
+
+sd.background_color = sd.COLOR_BLACK
+sd.resolution = (x, y)
 
 rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
                   sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
 
-a, b, c, d = 50, 50, 1100, 750
+a, b, c = 50, 50, x - 100
 for i in rainbow_colors * 30:
     point_start = sd.get_point(a, b)
-    point_finish = sd.get_point(c, d)
+    point_finish = sd.get_point(c, y - 50)
     step = 5
     sd.line(start_point=point_start, end_point=point_finish, color=i, width=4)
     a += step
     # b += step
-    c += step/12
+    c += step / 12
     # d += step
 
 # Усложненное задание, делать по желанию.
@@ -30,10 +34,7 @@ for i in rainbow_colors * 30:
 # def bubble(point, radius, color, width):
 #     sd.circle(center_position=point, radius=radius, width=width, color=color)
 
-
-x = 700
-y = -300
-point = sd.get_point(x, y)
+point = sd.get_point(x - 300, y - 1100)
 radius = 1050
 for i in rainbow_colors:
     # color = i
--- 3.1 Основные операторы/Доп. материалы/ДЗ 3/07_wall.py
+++ 3.1 Основные операторы/Доп. материалы/ДЗ 3/07_wall.py
@@ -1,32 +1,55 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
+
 import simple_draw as sd
-sd.background_color = sd.COLOR_DARK_PURPLE
+sd.background_color = sd.COLOR_WHITE
+
+x, y = 1200, 800
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
-sd.resolution = (1200, 800)
-x = 0
-a = 0
-step = 20
-y = step
-for i in range(0, sd.resolution[0] + 1, step):
-    start_point = sd.get_point(x, i)
-    stop_point = sd.get_point(sd.resolution[0], i)
-    sd.line(start_point=start_point, end_point=stop_point, color=sd.COLOR_WHITE, width=2)
 
-for a in range(x, sd.resolution[1], step * 2):
-    for j in range(0, sd.resolution[0], step * 2):
-        start_point2 = sd.get_point(j, a)
-        stop_point2 = sd.get_point(j, step + a)
-        sd.line(start_point=start_point2, end_point=stop_point2, color=sd.COLOR_WHITE, width=2)
+sd.resolution = (x, y)
 
-    for k in range(step, sd.resolution[0], step * 2):
-        start_point2 = sd.get_point(k, y + a)
-        stop_point2 = sd.get_point(k, y + step + a)
-        sd.line(start_point=start_point2, end_point=stop_point2, color=sd.COLOR_WHITE, width=2)
+# x = 0
+# a = 0
+# step = 20
+# y = step
+# for i in range(0, sd.resolution[0] + 1, step):
+#     start_point = sd.get_point(x, i)
+#     stop_point = sd.get_point(sd.resolution[0], i)
+#     sd.line(start_point=start_point, end_point=stop_point, color=sd.COLOR_WHITE, width=2)
+#
+# for a in range(x, sd.resolution[1], step * 2):
+#     for j in range(0, sd.resolution[0], step * 2):
+#         start_point2 = sd.get_point(j, a)
+#         stop_point2 = sd.get_point(j, step + a)
+#         sd.line(start_point=start_point2, end_point=stop_point2, color=sd.COLOR_WHITE, width=2)
+#
+#     for k in range(step, sd.resolution[0], step * 2):
+#         start_point2 = sd.get_point(k, y + a)
+#         stop_point2 = sd.get_point(k, y + step + a)
+#         sd.line(start_point=start_point2, end_point=stop_point2, color=sd.COLOR_WHITE, width=2)
+#
+#     a += step * 2
 
-    a += step * 2
+quantity_bricks = 25
 
+length_brick = x // quantity_bricks
+height_brick = int(length_brick / 2)
+height_wall = int((y // height_brick)*height_brick)
+
+print(length_brick, height_brick, height_wall)
+a = 1
+for i in range(0, height_wall, height_brick):
+    for j in range(- length_brick, x, length_brick):
+        if a % 2 != 0:
+            point_start = sd.get_point(j - 2, i - 2)
+            point_finish = sd.get_point(j + length_brick, i + height_brick)
+        else:
+            point_start = sd.get_point(j - 2 + length_brick / 2, i - 2)
+            point_finish = sd.get_point(j + length_brick * 1.5, i + height_brick)
+        sd.rectangle(left_bottom=point_start, right_top=point_finish, color=sd.COLOR_RED, width=2)
+    a += 1
 sd.pause()
