8f15779b6e43e69026999a41c01f10ca6fa46132
Alexander <AStrukov-96@yandex.ru>
2022-12-15 16:59:59 +0500

    Completed first task, started second task

--- 4.1. Функции подробнее/Доп. материалы/lesson_004/01_shapes_2.py
+++ 4.1. Функции подробнее/Доп. материалы/lesson_004/01_shapes_2.py
@@ -31,34 +31,33 @@ sd.background_color = sd.COLOR_DARK_CYAN
 # Результат решения см lesson_004/results/exercise_01_shapes.jpg
 
 
+def draw_polygon(start_point=sd.get_point(sd.resolution[0]/2, sd.resolution[1]/2),
+                 corners=4,
+                 angle=0, length_side=100, color=sd.COLOR_WHITE, width=2):
+    alpha = 360 / corners
+    betta = start_point
+    for i in range(corners - 1):
+        start_point = sd.vector(start=start_point, angle=angle, length=length_side, color=color, width=width)
+        angle += alpha
+    sd.line(start_point=start_point, end_point=betta, color=color, width=width)
+
+
 def draw_triangle(start_point, corner=30, length_side=150):
-    side = sd.get_vector(start_point=start_point, angle=corner, length=length_side, width=2)
-    side.draw(color=sd.COLOR_DARK_RED)
-    start_point_side_2 = side.end_point
+    side = sd.vector(start=start_point, angle=corner, length=length_side, width=2)
     corner += 360/3
-    side_2 = sd.get_vector(start_point=start_point_side_2, angle=corner, length=length_side, width=2)
-    side_2.draw(color=sd.COLOR_DARK_RED)
-    start_point_side_3 = side_2.end_point
+    side_2 = sd.vector(start=side, angle=corner, length=length_side, width=2)
     corner += 360/3
-    side_3 = sd.get_vector(start_point=start_point_side_3, angle=corner, length=length_side, width=2)
-    side_3.draw(color=sd.COLOR_DARK_RED)
+    sd.vector(start=side_2, angle=corner, length=length_side, width=2)
 
 
 def draw_square(start_point, corner=30, length_side=150):
     side = sd.vector(start=start_point, angle=corner, length=length_side, width=2)
-    # side.draw(color=sd.COLOR_DARK_RED)
-    start_point_side_2 = side
     corner += 360/4
-    side_2 = sd.vector(start=start_point_side_2, angle=corner, length=length_side, width=2)
-    # side_2.draw(color=sd.COLOR_DARK_RED)
-    start_point_side_3 = side_2
+    side_2 = sd.vector(start=side, angle=corner, length=length_side, width=2)
     corner += 360/4
-    side_3 = sd.vector(start=start_point_side_3, angle=corner, length=length_side, width=2)
-    # side_3.draw(color=sd.COLOR_DARK_RED)
-    start_point_side_4 = side_3
+    side_3 = sd.vector(start=side_2, angle=corner, length=length_side, width=2)
     corner += 360 / 4
-    side_4 = sd.vector(start=start_point_side_4, angle=corner, length=length_side, width=2)
-    # side_4.draw(color=sd.COLOR_DARK_RED)
+    sd.vector(start=side_3, angle=corner, length=length_side, width=2)
 
 
 def draw_pentagon(start_point, corner=30, length_side=150):
@@ -73,11 +72,55 @@ def draw_pentagon(start_point, corner=30, length_side=150):
     sd.line(start_point=side_4, end_point=start_point, color=sd.COLOR_DARK_RED, width=2)
 
 
+def draw_hexagon(start_point, corner=30, length_side=150, color=sd.COLOR_DARK_ORANGE, width=2):
+    side = sd.vector(start=start_point, angle=corner, length=length_side, color=color, width=width)
+    corner += 360 / 6
+    side_2 = sd.vector(start=side, angle=corner, length=length_side, color=color, width=width)
+    corner += 360 / 6
+    side_3 = sd.vector(start=side_2, angle=corner, length=length_side, color=color, width=width)
+    corner += 360 / 6
+    side_4 = sd.vector(start=side_3, angle=corner, length=length_side, color=color, width=width)
+    corner += 360 / 6
+    side_5 = sd.vector(start=side_4, angle=corner, length=length_side, color=color, width=width)
+    sd.line(start_point=side_5, end_point=start_point, color=color, width=width)
+
+
 point_0 = sd.get_point(300, 150)
-draw_triangle(start_point=point_0, corner=30)
 point_1 = sd.get_point(900, 150)
-draw_pentagon(start_point=point_1, corner=10)
 point_2 = sd.get_point(300, 450)
-draw_square(start_point=point_2, corner=30)
+point_3 = sd.get_point(900, 450)
+point_4 = sd.get_point(600, 250)
+
+# draw_triangle(start_point=point_0, corner=30)
+# draw_pentagon(start_point=point_1, corner=10)
+# draw_square(start_point=point_2, corner=30)
+# draw_hexagon(start_point=point_3, corner=30, length_side=100, color=sd.COLOR_DARK_ORANGE)
+
+# draw_polygon(start_point=point_4, corners=10, length_side=150)
+
+draw_polygon(start_point=point_0, corners=3, angle=25, length_side=200, color=sd.COLOR_RED)
+draw_polygon(start_point=point_1, corners=4, angle=25, length_side=200, color=sd.COLOR_RED)
+draw_polygon(start_point=point_2, corners=5, angle=25, length_side=200, color=sd.COLOR_RED)
+draw_polygon(start_point=point_3, corners=7, angle=25, length_side=150, color=sd.COLOR_RED)
+draw_polygon(start_point=point_4, corners=5)
+
+
+# Часть 2 (делается после зачета первой части)
+#
+# Надо сформировать функцию, параметризованную в местах где была "небольшая правка".
+# Это называется "Выделить общую часть алгоритма в отдельную функцию"
+# Потом надо изменить функции рисования конкретных фигур - вызывать общую функцию вместо "почти" одинакового кода.
+#
+# В итоге должно получиться:
+#   - одна общая функция со множеством параметров,
+#   — все функции отрисовки треугольника/квадрата/етс берут 3 параметра и внутри себя ВЫЗЫВАЮТ общую функцию.
+#
+# Не забудьте в этой общей функции придумать, как устранить разрыв
+#   в начальной/конечных точках рисуемой фигуры (если он есть)
+
+# Часть 2-бис.
+# А теперь - сколько надо работы, чтобы внести изменения в код? Выгода на лицо
+# Поэтому среди программистов есть принцип D.R.Y. https://clck.ru/GEsA9
+# Будьте ленивыми, не используйте копи-пасту!
 
 sd.pause()
--- 4.1. Функции подробнее/Доп. материалы/lesson_004/probe_1.py
+++ 4.1. Функции подробнее/Доп. материалы/lesson_004/probe_1.py
@@ -2,33 +2,12 @@ import simple_draw as sd
 
 sd.resolution = (1200, 800)
 
-
-def draw_the_wall(inc_point_x, inc_point_y, width, step, heihgt):
-    '''Рисует кирпичную стену по заданным размерам стены'''
-    width_1 = width % (step * 2)
-    width_wall = width - width_1
-    y = step
-    a = 0
-    b = 0
-    for i in range(inc_point_y, heihgt + inc_point_y + 1, step):
-        start_point = sd.get_point(inc_point_x, i)
-        stop_point = sd.get_point(width_wall + inc_point_x, i)
-        sd.line(start_point=start_point, end_point=stop_point, color=sd.COLOR_WHITE, width=2)
-
-    for a in range(inc_point_y, heihgt + inc_point_y, step * 2):
-        for j in range(inc_point_x, width_wall + inc_point_x + step, step * 2):
-            start_point2 = sd.get_point(j, a)
-            stop_point2 = sd.get_point(j, a + step)
-            sd.line(start_point=start_point2, end_point=stop_point2, color=sd.COLOR_WHITE, width=2)
-
-        for k in range(inc_point_x + step, width_wall + inc_point_x, step * 2):
-            if a + step >= heihgt + inc_point_x:
-                break
-            start_point1 = sd.get_point(k, a + step)
-            stop_point1 = sd.get_point(k, a + step * 2)
-            sd.line(start_point=start_point1, end_point=stop_point1, color=sd.COLOR_WHITE, width=2)
-
-
-draw_the_wall(inc_point_x=350, inc_point_y=250, width=740, step=30, heihgt=300)
+point = sd.get_point(600, 200)
+side = sd.get_vector(start_point=point, angle=90, length=100)
+side_2 = sd.get_vector(start_point=side.end_point, angle=180, length=200)
+# side.draw()
+# side_2.draw()
+side_3 = side.add(side_2)
+print(side.__str__())
 
 sd. pause()
