aa12ba48e2a76c252d4dbeec8fb0c88c0a3b5bd5
AStru <AStrukov-96@yandex.ru>
2022-12-15 22:22:35 +0500

    Completed second task

--- 4.1. Функции подробнее/Доп. материалы/lesson_004/02_global_color_02.py
+++ 4.1. Функции подробнее/Доп. материалы/lesson_004/02_global_color_02.py
@@ -5,15 +5,17 @@ import simple_draw as sd
 sd.resolution = (1200, 800)
 sd.background_color = sd.COLOR_DARK_CYAN
 
-COLORS = (sd.COLOR_WHITE,
-          sd.COLOR_BLACK,
-          sd.COLOR_RED,
-          sd.COLOR_ORANGE,
-          sd.COLOR_YELLOW,
-          sd.COLOR_GREEN,
-          sd.COLOR_CYAN,
-          sd.COLOR_BLUE,
-          sd.COLOR_PURPLE)
+COLORS = (
+    sd.COLOR_WHITE,
+    sd.COLOR_BLACK,
+    sd.COLOR_RED,
+    sd.COLOR_ORANGE,
+    sd.COLOR_YELLOW,
+    sd.COLOR_GREEN,
+    sd.COLOR_CYAN,
+    sd.COLOR_BLUE,
+    sd.COLOR_PURPLE
+    )
 
 # Добавить цвет в функции рисования геометрических фигур из упр lesson_004/01_shapes.py
 # (код функций скопировать сюда и изменить)
@@ -28,6 +30,7 @@ COLORS = (sd.COLOR_WHITE,
 # и константы COLOR_RED, COLOR_ORANGE, COLOR_YELLOW, COLOR_GREEN, COLOR_CYAN, COLOR_BLUE, COLOR_PURPLE
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
 
+
 def draw_polygon(start_point=sd.get_point(sd.resolution[0]/2, sd.resolution[1]/2),
                  corners=4,
                  angle=0, length_side=100, color=sd.COLOR_WHITE, width=2):
@@ -39,31 +42,42 @@ def draw_polygon(start_point=sd.get_point(sd.resolution[0]/2, sd.resolution[1]/2
     sd.line(start_point=start_point, end_point=betta, color=color, width=width)
 
 
-custom_color = None
-while not isinstance(custom_color, int):
+print(
+    f'''Список возможных цветов фигур: \n
+      0 - white
+      1 - black
+      2 - red
+      3 - orange
+      4 - yellow
+      5 - green
+      6 - cyan
+      7 - blue
+      8 - purple'''
+      )
 
-    input("Введите выбранный цвет: ")
-    if not isinstance(custom_color, int):
-        print('Вы ввели не число')
+
+custom_color = int(input('Выберите цвет отображения фигур: '))
+
+if custom_color < 0:
+    print('Вы ввели слишком маленькое значение!!!')
+elif custom_color > len(COLORS) - 1:
+    print('Вы ввели слишком большое значение!!!')
 
 
+print(type(custom_color))
 
-
-
-
-
-
+color_figure = COLORS[custom_color]
 point_0 = sd.get_point(300, 100)
 point_1 = sd.get_point(900, 100)
 point_2 = sd.get_point(300, 450)
 point_3 = sd.get_point(900, 450)
 point_4 = sd.get_point(600, 350)
 
-draw_polygon(start_point=point_0, corners=3, angle=25, length_side=200, color=sd.COLOR_RED)
-draw_polygon(start_point=point_1, corners=4, angle=25, length_side=200, color=sd.COLOR_RED)
-draw_polygon(start_point=point_2, corners=5, angle=25, length_side=200, color=sd.COLOR_RED)
-draw_polygon(start_point=point_3, corners=7, angle=25, length_side=150, color=sd.COLOR_RED)
-draw_polygon(start_point=point_4, corners=5)
+draw_polygon(start_point=point_0, corners=3, angle=25, length_side=200, color=color_figure)
+draw_polygon(start_point=point_1, corners=4, angle=25, length_side=200, color=color_figure)
+draw_polygon(start_point=point_2, corners=5, angle=25, length_side=200, color=color_figure)
+draw_polygon(start_point=point_3, corners=7, angle=25, length_side=150, color=color_figure)
+draw_polygon(start_point=point_4, corners=5, color=color_figure)
 
 
 sd.pause()
