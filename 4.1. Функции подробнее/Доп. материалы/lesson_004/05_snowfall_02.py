144abeb004e7713b1ee2df3e30f6079e9881203d
AStru <AStrukov-96@yandex.ru>
2022-12-18 00:04:58 +0500

    Modification task fifth (snowfall). Added some functions.

--- 4.1. Функции подробнее/Доп. материалы/lesson_004/05_snowfall_02.py
+++ 4.1. Функции подробнее/Доп. материалы/lesson_004/05_snowfall_02.py
@@ -3,14 +3,53 @@ import random as rd
 import simple_draw as sd
 
 sd.resolution = (1200, 800)
-sd.background_color = sd.COLOR_PURPLE
+sd.background_color = sd.COLOR_DARK_CYAN
 
 # На основе кода из практической части реализовать снегопад:
 # - создать списки данных для отрисовки N снежинок
 # - нарисовать падение этих N снежинок
 # - создать список рандомных длин лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
 
-N = 20
+N = 25
+
+
+def initial_snowfall(quantity):
+
+    """
+    Создаёт словарь списков координат и рандомной длины лучей новых снежинок.
+    Принимает количество снежинок
+    """
+    dictionary_of_new_snowflake = {}
+    for k in range(quantity):
+        x = rd.randint(50, 1150)
+        y = rd.randint(400, 750)
+        length_of_branch = rd.randint(20, 60)
+        coordinates_of_new_snowfall = [x, y, length_of_branch]
+        dictionary_of_new_snowflake[k] = coordinates_of_new_snowfall
+    return dictionary_of_new_snowflake
+
+
+def change_coordinates_of_an_existing_snowflake(list_of_coordinates):
+
+    """
+    Принимает словарь координат снежинок, проверяет, достигли ли они нижнего края экрана.
+    Если снежинка достигла нижней границы экрана, создаёт новую в верхней части экрана.
+    :return: словарь снежинок.
+    """
+    list_of_coordinates[0] = rd.randint(50, 1150)
+    list_of_coordinates[2] = rd.randint(10, 60)
+    list_of_coordinates[1] = 800 - list_of_coordinates[2]
+
+    return list_of_coordinates
+
+
+def drawing_snowflake(list_of_coordinates, color=sd.COLOR_WHITE):
+
+    """Рисует снежинку. Принимает список координат, длину лучиков и цвет."""
+
+    point_of_snowflake = sd.get_point(list_of_coordinates[0], list_of_coordinates[1])
+    sd.snowflake(center=point_of_snowflake, length=list_of_coordinates[2], color=color)
+
 
 # Пригодятся функции
 # sd.get_point()
@@ -19,69 +58,33 @@ N = 20
 # sd.random_number()
 # sd.user_want_exit()
 
-initial_coordinates = {}
 
-for i in range(N):
-    x = rd.randint(50, 1150)
-    y = rd.randint(400, 750)
-    length_of_branch = rd.randint(20, 60)
-    coordinates = [x, y, length_of_branch]
-    initial_coordinates[i] = coordinates
-
-second_coordinates = initial_coordinates.copy()
+initial_coordinates = initial_snowfall(N)
 
 while True:
     sd.start_drawing()
 
-    for i in initial_coordinates:
-        point = sd.get_point(initial_coordinates[i][0], initial_coordinates[i][1])
-        sd.snowflake(center=point, length=initial_coordinates[i][2])
+    for j in initial_coordinates:
+        drawing_snowflake(initial_coordinates[j])
 
-    # second_coordinates = initial_coordinates.copy()
     sd.finish_drawing()
 
     sd.sleep(0.1)
     sd.start_drawing()
-    # for j in initial_coordinates:
-    #     if initial_coordinates[j][1] > initial_coordinates[j][2]:
-    #         point = sd.get_point(initial_coordinates[j][0], initial_coordinates[j][1])
-    #         sd.snowflake(center=point, length=initial_coordinates[j][2], color=sd.background_color)
-    for i in initial_coordinates:
-        if initial_coordinates[i][1] <= initial_coordinates[i][2]:
-            initial_coordinates[i][1] = 800 - initial_coordinates[i][2]
-            initial_coordinates[i][0] = rd.randint(50, 1150)
+
+    for j in initial_coordinates:
+        if initial_coordinates[j][1] <= initial_coordinates[j][2]:
+            initial_coordinates[j] = change_coordinates_of_an_existing_snowflake(initial_coordinates[j])
         else:
-            point = sd.get_point(initial_coordinates[i][0], initial_coordinates[i][1])
-            sd.snowflake(center=point, length=initial_coordinates[i][2], color=sd.background_color)
-            initial_coordinates[i][0] += rd.randint(-10, 10)
-            initial_coordinates[i][1] -= rd.randint(2, 20)
+            drawing_snowflake(initial_coordinates[j], color=sd.background_color)
+
+            initial_coordinates[j][0] += rd.randint(-10, 10)
+            initial_coordinates[j][1] -= rd.randint(2, 20)
 
     sd.finish_drawing()
 
     if sd.user_want_exit():
         break
-
-
-# while True:
-#     sd.clear_screen()
-#     point = sd.get_point(x, y)
-#     sd.snowflake(center=point, length=50)
-#     y -= 10
-#     if y < 50:
-#         break
-#     x = x + 7
-#
-#     point2 = sd.get_point(x2, y2)
-#     sd.snowflake(center=point2, length=30)
-#     y2 -= 10
-#     if y2 < 50:
-#         break
-#     x2 = x2 + 12
-#
-#     sd.sleep(0.1)
-#     if sd.user_want_exit():
-#         break
-
 
 # подсказка! для ускорения отрисовки можно
 #  - убрать clear_screen()
--- 4.1. Функции подробнее/Доп. материалы/lesson_004/probe_1.py
+++ 4.1. Функции подробнее/Доп. материалы/lesson_004/probe_1.py
@@ -1,13 +1,1 @@
-import simple_draw as sd
 
-sd.resolution = (1200, 800)
-
-point = sd.get_point(600, 200)
-side = sd.get_vector(start_point=point, angle=90, length=100)
-side_2 = sd.get_vector(start_point=side.end_point, angle=180, length=200)
-# side.draw()
-# side_2.draw()
-side_3 = side.add(side_2)
-print(side.__str__())
-
-sd. pause()
