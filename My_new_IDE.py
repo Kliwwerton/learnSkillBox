81c9d6e0ace5ee5c60bdb0ea7b4ec12c4d912c67
AStru <AStrukov-96@yandex.ru>
2022-12-28 23:29:23 +0500

    Added button "Press me"

--- My_new_IDE.py
+++ My_new_IDE.py
@@ -6,19 +6,26 @@
 # window.mainloop()
 
 from kivy.app import App
-# from kivy.uix.label import Label
+from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+
+
+def on_press_button(instance):
+    print('Вы нажали на кнопку!')
 
 
 class My_windowApp(App):
     def build(self):
         layout = BoxLayout(padding=10)
-        # label = Label(text='Hello kivy', size_hint=(1, 1))
+        label = Label(text='Hello kivy', size_hint=(1, 1))
         btn = Button(text='Press Me', background_color=[1, 0, 0, 1], )
 
-        layout.add_widget(btn)
-        return layout
+        # layout.add_widget(btn)
+        layout.add_widget(label)
+        btn.bind(on_press=on_press_button)
+
+        return btn
 
 
 if __name__ == '__main__':
