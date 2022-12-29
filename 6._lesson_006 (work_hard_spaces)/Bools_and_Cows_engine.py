07d3320543ecf7e5070b8925332ac33ebcf89e6d
AStru <AStrukov-96@yandex.ru>
2022-12-29 23:09:44 +0500

    Created Bools_and_Cows_engine.py

---
+++ 6._lesson_006 (work_hard_spaces)/Bools_and_Cows_engine.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+_number = 0
+
+
+def create_number():
+
+    """ Computer create the number """
+
+    pass
+
+
+def create_user_number():
+
+    """ Create user number and return it """
+
+    pass
+
+
+def access_number_and_check(computer_number, user_number):
+
+    """ Computer check the number and returns quantity Bools and Cows """
+
+    pass
+
+
+def defines_is_win():
+
+    """ defines win or not """
+
+    pass
