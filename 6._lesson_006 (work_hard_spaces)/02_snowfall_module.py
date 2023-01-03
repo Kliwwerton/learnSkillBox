fe7c5524612931524e08ebfede1200c5e38e9770
AStru <AStrukov-96@yandex.ru>
2023-01-03 22:21:17 +0500

    Created new function drawing_snowflake

--- 6._lesson_006 (work_hard_spaces)/02_snowfall_module.py
+++ 6._lesson_006 (work_hard_spaces)/02_snowfall_module.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import simple_draw as sd
+import snowfall
 
 # На основе кода из lesson_004/05_snowfall.py
-# сделать модуль snowfall.py в котором реализовать следующие функции:
+# сделать модуль snowfall.py, в котором реализовать следующие функции:
 #    создать_снежинки(N) - создает N снежинок
 #    нарисовать_снежинки_цветом (color) - отрисовывает все снежинки цветом color
 #    сдвинуть_снежинки() - сдвигает снежинки на один шаг
@@ -14,7 +15,12 @@ import simple_draw as sd
 # обращаясь ТОЛЬКО к функциям модуля snowfall
 
 # создать_снежинки(N)
+n = 50
+snowflakes = snowfall.initial_snowfall(quantity=n,
+                                       min_point=sd.get_point(20, 20),
+                                       max_point=sd.get_point(sd.resolution[0]-20, sd.resolution[1]-20))
 while True:
+    snowfall.drawing_snowflake(dict_of_coordinates=snowflakes)
     #  нарисовать_снежинки_цветом(color=sd.background_color)
     #  сдвинуть_снежинки()
     #  нарисовать_снежинки_цветом(color)
--- 6._lesson_006 (work_hard_spaces)/snowfall.py
+++ 6._lesson_006 (work_hard_spaces)/snowfall.py
@@ -4,10 +4,9 @@ import simple_draw as sd
 import random as rd
 
 sd.resolution = (1200, 800)
-N = 50  # количество снежинок
 
 
-def initial_snowfall(quantity, min_point, max_point, color=sd.COLOR_WHITE):
+def initial_snowfall(quantity, min_point, max_point):
 
     """
     Создаёт словарь списков координат и рандомной длины лучей новых снежинок.
@@ -21,15 +20,25 @@ def initial_snowfall(quantity, min_point, max_point, color=sd.COLOR_WHITE):
         x = rd.randint(min_point.x + length_of_branch, max_point.x)
         y = rd.randint(min_point.y + length_of_branch, max_point.y)
         dictionary_of_new_snowflakes[k] = [x, y, length_of_branch]
-        sd.snowflake(center=sd.get_point(x, y), length=length_of_branch, color=color)
+
     return dictionary_of_new_snowflakes
 
 
-def main():
+def drawing_snowflake(dict_of_coordinates, color=sd.COLOR_WHITE):
 
+    """Рисует снежинку. Принимает словарь из списка координат и длины лучей и цвет."""
+
+    for i in dict_of_coordinates:
+        point_of_snowflake = sd.get_point(dict_of_coordinates[i][0], dict_of_coordinates[i][1])
+        sd.snowflake(center=point_of_snowflake, length=dict_of_coordinates[i][2], color=color)
+
+
+def main():
+    n = 50  # количество снежинок
     min_point = sd.get_point(20, 20)
     max_point = sd.get_point(sd.resolution[0]-20, sd.resolution[1]-20)
-    snowflakes = initial_snowfall(quantity=N, min_point=min_point, max_point=max_point)
+    snowflakes = initial_snowfall(quantity=n, min_point=min_point, max_point=max_point)
+
     while True:
 
         if sd.user_want_exit(sleep_time=0.1):
