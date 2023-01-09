cff7a8f85709d23bd2cbfea19234e2f629dab237
AStru <AStrukov-96@yandex.ru>
2023-01-10 00:06:09 +0500

    Created new classes

--- 7._lesson_007 (classes)/02_alchemy.py
+++ 7._lesson_007 (classes)/02_alchemy.py
@@ -19,7 +19,74 @@
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
+
+class Water:
+
+    def __add__(self, other):
+        if isinstance(other, (Air, Fire, Earth)):
+            if other == Fire:
+                return Storm()
+
+    def __str__(self):
+        return 'Вода'
+
+
+class Air:
+
+    def __add__(self, other):
+        pass
+
+
+class Fire:
+
+    def __add__(self, other):
+        if isinstance(other, (Water, Air, Earth)):
+            if other == Water:
+                return Storm()
+        else:
+            print("Не относится ни к одному из классов!")
+
+    def __str__(self):
+        return 'Огонь'
+
+
+class Earth:
+
+    def __add__(self, other):
+        pass
+
+
+class Storm:
+    title = 'Шторм'
+
+    def __str__(self):
+        return 'Шторм'
+
+
+class Steam:
+    def __str__(self):
+        return 'Пар'
+
+
+class Dirt:
+    def __str__(self):
+        return 'Грязь'
+
+
+def main():
+    water = Water()
+    earth = Earth()
+    fire = Fire()
+    air = Air()
+    # storm = Storm()
+    iq1 = water + fire
+    print(iq1)
+    print(fire)
+
+
+if __name__ == '__main__':
+    main()
+
 
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
