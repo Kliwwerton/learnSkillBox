9bf673bfc9a6034c19c47c271f050342a821e6df
AStru <AStrukov-96@yandex.ru>
2023-01-10 23:16:31 +0500

    Started task 7.2

--- 7._lesson_007 (classes)/03_man_ans_cat.py
+++ 7._lesson_007 (classes)/03_man_ans_cat.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import random as rd
+from termcolor import cprint
 # Доработать практическую часть урока lesson_007/python_snippets/08_practice.py
 
 # Необходимо создать класс кота. У кота есть аттрибуты - сытость и дом (в котором он живет).
@@ -23,7 +25,97 @@
 
 # Человеку и коту надо вместе прожить 365 дней.
 
-# TODO здесь ваш код
+
+class Man:
+
+    def __init__(self, name):
+        self.name = name
+        self.fullness = 50
+        self.house = None
+
+    def __str__(self):
+        return 'Я - {}, сытость {}'.format(
+            self.name, self.fullness)
+
+    def eat(self):
+        if self.house.food >= 10:
+            cprint('{} поел'.format(self.name), color='yellow')
+            self.fullness += 10
+            self.house.food -= 10
+        else:
+            cprint('{} нет еды'.format(self.name), color='red')
+
+    def work(self):
+        cprint('{} сходил на работу'.format(self.name), color='blue')
+        self.house.money += 50
+        self.fullness -= 10
+
+    def watch_mtv(self):
+        cprint('{} смотрел MTV целый день'.format(self.name), color='green')
+        self.fullness -= 10
+
+    def shopping(self):
+        if self.house.money >= 50:
+            cprint('{} сходил в магазин за едой'.format(self.name), color='magenta')
+            self.house.money -= 50
+            self.house.food += 50
+        else:
+            cprint('{} деньги кончились!'.format(self.name), color='red')
+
+    def go_to_the_house(self, house):
+        self.house = house
+        self.fullness -= 10
+        cprint('{} Вьехал в дом'.format(self.name), color='cyan')
+
+    def act(self):
+        if self.fullness <= 0:
+            cprint('{} умер...'.format(self.name), color='red')
+            return
+        dice = rd.randint(1, 6)
+        if self.fullness < 20:
+            self.eat()
+        elif self.house.food < 10:
+            self.shopping()
+        elif self.house.money < 50:
+            self.work()
+        elif dice == 1:
+            self.work()
+        elif dice == 2:
+            self.eat()
+        else:
+            self.watch_mtv()
+
+
+class House:
+
+    def __init__(self):
+        self.food = 50
+        self.money = 0
+
+    def __str__(self):
+        return 'В доме еды осталось {}, денег осталось {}'.format(
+            self.food, self.money)
+
+
+citizens = [
+    Man(name='Бивис'),
+    Man(name='Батхед'),
+    Man(name='Кенни'),
+]
+
+
+my_sweet_home = House()
+for citizen in citizens:
+    citizen.go_to_the_house(house=my_sweet_home)
+
+for day in range(1, 366):
+    print('================ день {} =================='.format(day))
+    for citizen in citizens:
+        citizen.act()
+    print('--- в конце дня ---')
+    for citizen in citizens:
+        print(citizen)
+    print(my_sweet_home)
 
 # Усложненное задание (делать по желанию)
 # Создать несколько (2-3) котов и подселить их в дом к человеку.
