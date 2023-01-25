844c03f2cf49546f7cb767f01c7731b85aff986e
AStru <AStrukov-96@yandex.ru>
2023-01-25 23:13:32 +0500

    Added new buttons (Справочника, Нормативы)

--- My_new_IDE.py
+++ My_new_IDE.py
@@ -9,41 +9,64 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
 
 
-def on_press_button(instance):
+def on_press_button(a):
+    new_window = Popup(title='Расчёт давления прессования')
     print('Вы нажали на кнопку!')
 
 
-def on_press_button_2(instance):
+def on_press_button_2(a):
     print('Вы нажали на кнопку 2!')
+
+
+def on_press_button_3(a):
+    print('Вы нажали "СПРАВОЧНИКИ"!')
+
+
+def on_press_button_4(a):
+    print('Вы нажали на кнопку "Нормативы"!')
 
 
 class My_windowApp(App):
     def build(self):
-        layout = BoxLayout(padding=50)
-        label = Label(text='Привет Китти',
-                      size_hint=(0.2, 0.2),
-                      pos_hint={'center_x': 0.1, 'center_y': 0.9})
+        layout = BoxLayout(padding=50, orientation='vertical')
+        label = Label(text='Привет Юный ИНЖЕНЕР!', font_size=55,
+                      size_hint=(0.2, 0.1),
+                      pos_hint={'center_x': 0.5, 'center_y': 1})
 
-        btn = Button(text='Press Me',
-                     size_hint=(0.2, 0.25),
+        btn = Button( font_size=25,
+                     size_hint=(0.6, 0.1),
                      pos_hint={'center_x': 0.5, 'center_y': 0.5},
-                     background_color=[0.5, 1, 1, 1])
-        btn_2 = Button(text='Press my too',
-                       size_hint=(0.2, 0.25),
-                       pos_hint={'center_x': 0.7, 'center_y': 0.5},
+                     background_color=[0.5, 1, 1, 1],
+                     text=f'{"Расчёт давления прессования": ^100}')
+        btn_2 = Button(text='Расчёт \nхимического состава',
+                       size_hint=(0.6, 0.1),
+                       pos_hint={'center_x': 0.5, 'center_y': 0.5},
                        background_color=[0.5, 1, 0.5, 1])
+        btn_3 = Button(text='Справочник\n(ГОСТы, Классы шамота',
+                       size_hint=(0.6, 0.1), font_size=25,
+                       pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        btn_4 = Button(text='Нормативы брака (по переделам)',
+                       font_size=25,
+                       size_hint=(0.6, 0.1),
+                       pos_hint={'center_x': 0.5, 'center_y': 0.5})
 
         layout.add_widget(label)
         layout.add_widget(btn)
         layout.add_widget(btn_2)
+        layout.add_widget(btn_3)
+        layout.add_widget(btn_4)
         btn.bind(on_press=on_press_button)
         btn_2.bind(on_press=on_press_button_2)
+        btn_3.bind(on_press=on_press_button_3)
+        btn_4.bind(on_press=on_press_button_4)
+
 
         return layout
 
 
 if __name__ == '__main__':
-    my_window = My_windowApp()
+    my_window = My_windowApp(title='Я ИНЖЕНЕР!')
     my_window.run()
